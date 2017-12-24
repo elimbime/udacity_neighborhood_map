11482631e160e98a58777d162a6a48fd70bf9225
Unknown <ivan.elimbi@acensi.fr>
2017-12-24 01:42:42 +0100

    Ajout gestion api
    -Wikiepedia
    -New-York Times

--- script/site.js
+++ script/site.js
@@ -1,18 +1,94 @@
 var markersInit = [{
-        position: {
-            lat: 48.862592,
-            lng: 2.3512000000000626
-        },
-        title: 'Anti Cafe Beaubourg'
+    position: {
+        lat: 48.862592,
+        lng: 2.3512000000000626
     },
-    {
-        position: {
-            lat: 48.8626116,
-            lng: 2.3520730000000185
-        },
-        title: 'Workshop Paris'
+    title: 'Anti Cafe Beaubourg'
+},
+{
+    position: {
+        lat: 48.8626116,
+        lng: 2.3520730000000185
     },
+    title: 'Workshop Paris'
+},
 ];
+
+
+function getWikipediaInformation() {
+    var urlWikipdedia = "https://en.wikipedia.org/w/api.php";
+
+    var paramsWikipedia = {
+        'action': 'opensearch',
+        'search': city,
+        'format': 'json',
+    };
+
+    $.ajax({
+        dataType: "jsonp",
+        url: urlWikipdedia,
+        data: paramsWikipedia,
+        success: function (data) {
+            var articles = data[1];
+            var artilcesLinks = data[3];
+            var length = articles.length;
+            for (var index = 0; index < length; index++) {
+                $wikiElem.append('<li><a href="' + artilcesLinks[index] + '">' + articles[index] + '</a></li>');
+            }
+        }
+    }).error(function () {
+        $greeting.text("An error occured during your Ajax call !!!!");
+    });
+
+}
+
+function getTimeInformation() {
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // YOUR CODE GOES HERE!  
+    $greeting.text("You want live in " + city);
+
+    var urlNyt = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+    var params = {
+        'api-key': "843ec920599e452fa83ab934c7ebb0f6",
+        'q': city
+    };
+
+    $.getJSON(urlNyt, params, function (data) {
+        $nytHeaderElem.text("New York Times Articles About " + city);
+        var docs = data.response.docs;
+        var length = docs.length;
+        for (var index = 0; index < length; index++) {
+            var doc = docs[index];
+            $nytElem.append('<li class="article">' +
+                '<a href="' + doc.web_url + '">' + doc.headline.main + '</a>' +
+                '<p>' + doc.snippet + '</p>' +
+                '</li>');
+        }
+    }).error(function () {
+        $greeting.text("An error occured during your Ajax call !!!!");
+    });
+}
+
+function getFlickrInformation() {
+
+}
+
+
+
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
 
 function initMap() {
     var anticafe = {
@@ -37,6 +113,7 @@ function initMap() {
             title: curMarker.title,
             map: map
         });
+        marker.addListener('click', toggleBounce);
     }
     var infowindow = new google.maps.InfoWindow({
         content: '<h1>Some stylish comment</h1>'
@@ -52,3 +129,4 @@ var myNeighborhoodView = {
 document.addEventListener("DOMContentLoaded", function (event) {
     ko.applyBindings(myNeighborhoodView);
 });
+
